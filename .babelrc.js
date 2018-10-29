2ca3672585ff965d1285d912856dd71dd61dc307
GarethSmall <gareth@chartlytics.com>
2018-10-28 20:27:43 -0700

    updated dev files

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,31 @@
-{
-  "presets": ["@babel/env", "@babel/preset-flow", "@babel/preset-react"],
-  "plugins": [
-    "@babel/plugin-proposal-export-namespace-from",
-    ["@babel/plugin-transform-runtime",   {
-      "corejs": false,
-      "helpers": true,
-      "regenerator": true,
-      "useESModules": true,
-    }],
-    plugins: [
-      '@babel/proposal-object-rest-spread',
-      NODE_ENV === 'test' && '@babel/transform-modules-commonjs'
-    ].filter(Boolean)
-  ]
+const {NODE_ENV} = process.env;
+
+const presets = [
+  [
+    '@babel/env',
+    {
+      targets: {
+        browsers: ['ie >= 11']
+      },
+      exclude: ['transform-async-to-generator', 'transform-regenerator'],
+      modules: false,
+      loose: true
+    }
+  ],
+  '@babel/preset-flow',
+  '@babel/preset-react',
+];
+
+const plugins = [
+  '@babel/plugin-proposal-export-namespace-from',
+  '@babel/proposal-object-rest-spread',
+];
+
+if (NODE_ENV === 'test') {
+  plugins.push('@babel/transform-modules-commonjs')
 }
+
+module.exports = {
+  presets,
+  plugins,
+};
